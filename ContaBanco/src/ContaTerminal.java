3ee798c2a61ec71858d4a08137ce165983540b7c
Max Sousa <max.yuri.13@hotmail.com>
2024-07-02 17:10:15 -0300

    add getters and setters methods

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -12,6 +12,38 @@ public class ContaTerminal {
         this.saldo = saldo;
     }
 
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
     public static void main(String[] args) {
         // ContaBancaria conta = new ContaBancaria();
         // System.out.println(conta);
