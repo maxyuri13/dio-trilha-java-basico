9dff26a3bce9a1aab5b53c293cda2cf62065594b
Max Sousa <max.yuri.13@hotmail.com>
2024-07-02 17:08:04 -0300

    add constructor

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -5,7 +5,15 @@ public class ContaTerminal {
     private String nomeCliente;
     private double saldo;
 
-    public static void main(String[] args) throws Exception {
-        
+    public ContaBancaria(int numero, String agencia, String nomeCliente, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public static void main(String[] args) {
+        // ContaBancaria conta = new ContaBancaria();
+        // System.out.println(conta);
     }
 }
