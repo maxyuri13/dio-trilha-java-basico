130ade1f3216b37bd3a0f168510c5c6df614949f
Max Sousa <max.yuri.13@hotmail.com>
2024-07-06 16:21:41 -0300

    Implemented classes and interfaces with functionality tests

--- POO/src/Iphone.java
+++ POO/src/Iphone.java
@@ -1,3 +1,70 @@
-public class Iphone {
-  
+public class iPhone implements MusicPlayer, Phone, WebBrowser {
+    
+  // MusicPlayer methods
+  @Override
+  public void play() {
+      System.out.println("Playing music...");
+  }
+
+  @Override
+  public void pause() {
+      System.out.println("Music paused.");
+  }
+
+  @Override
+  public void selectMusic(String music) {
+      System.out.println("Selecting music: " + music);
+  }
+
+  // Phone methods
+  @Override
+  public void call(String number) {
+      System.out.println("Calling " + number + "...");
+  }
+
+  @Override
+  public void answer() {
+      System.out.println("Answering the call.");
+  }
+
+  @Override
+  public void startVoicemail() {
+      System.out.println("Starting voicemail.");
+  }
+
+  // WebBrowser methods
+  @Override
+  public void displayPage(String url) {
+      System.out.println("Displaying page: " + url);
+  }
+
+  @Override
+  public void addNewTab() {
+      System.out.println("Adding new tab.");
+  }
+
+  @Override
+  public void refreshPage() {
+      System.out.println("Refreshing page.");
+  }
+
+  public static void main(String[] args) {
+      iPhone myiPhone = new iPhone();
+      
+      // Testing Music Player
+      myiPhone.selectMusic("My Favorite Song");
+      myiPhone.play();
+      myiPhone.pause();
+      
+      // Testing Phone functionality
+      myiPhone.call("9999-9999");
+      myiPhone.answer();
+      myiPhone.startVoicemail();
+      
+      // Testing Web Browser
+      myiPhone.displayPage("www.dio.com");
+      myiPhone.addNewTab();
+      myiPhone.refreshPage();
+  }
 }
+
